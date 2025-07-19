eb78cdd80b3845704c156833b3754d95d0ab8cde
cml1010101 <cml1010101@gmail.com>
2025-07-18 21:56:49 -0400

    Preliminary drivetrain...

---
+++ src/main/cpp/subsystems/SwerveDrive.cpp
@@ -0,0 +1,187 @@
+#include "subsystems/SwerveDrive.h"
+#include <frc/DriverStation.h>
+#include <frc/RobotController.h>
+#include <frc/MathUtil.h>
+
+using namespace nfr;
+using namespace ctre::phoenix6;
+using namespace ctre::phoenix6::swerve;
+using namespace ctre::phoenix6::hardware;
+using namespace frc2::sysid;
+using namespace frc2;
+using namespace frc;
+using namespace units;
+using namespace std;
+using namespace pathplanner;
+using namespace choreo;
+
+SwerveDrive::SwerveDrive(const SwerveDrivetrainConstants &driveConstants,
+                         hertz_t updateRate,
+                         std::array<double, 3> const &odometryStandardDeviation,
+                         std::array<double, 3> const &visionStandardDeviation,
+                         PIDConstants translationPID,
+                         PIDConstants rotationPID,
+                         const SwerveModuleConstants & frontLeftConstants,
+                         const SwerveModuleConstants & frontRightConstants,
+                         const SwerveModuleConstants & rearLeftConstants,
+                         const SwerveModuleConstants & rearRightConstants)
+    : SwerveDrivetrain(driveConstants, updateRate, odometryStandardDeviation, visionStandardDeviation, frontLeftConstants,
+        frontRightConstants, rearLeftConstants, rearRightConstants)
+{
+    configurePathplanner(translationPID, rotationPID);
+    configureChoreo(translationPID, rotationPID);
+    if (utils::IsSimulation())
+    {
+        startSimThread();
+    }
+}
+
+void SwerveDrive::configurePathplanner(PIDConstants translationPID,
+                                       PIDConstants rotationPID)
+{
+    auto config = RobotConfig::fromGUISettings();
+    AutoBuilder::configure(
+        [this]()
+        { return GetState().Pose; },
+        [this](const Pose2d &pose)
+        { ResetPose(pose); },
+        [this]()
+        { return GetState().Speeds; },
+        [this](const ChassisSpeeds &speeds, const DriveFeedforwards &feedforwards)
+        {
+            SetControl(pathplannerFollower
+                           .WithSpeeds(speeds)
+                           .WithWheelForceFeedforwardsX(feedforwards.robotRelativeForcesX)
+                           .WithWheelForceFeedforwardsY(feedforwards.robotRelativeForcesY));
+        },
+        make_shared<PPHolonomicDriveController>(
+            translationPID,
+            rotationPID),
+        std::move(config),
+        []
+        {
+            auto const alliance = DriverStation::GetAlliance().value_or(DriverStation::Alliance::kBlue);
+            return alliance == DriverStation::Alliance::kRed;
+        },
+        this);
+}
+
+void SwerveDrive::startSimThread()
+{
+    lastSimTime = utils::GetCurrentTime();
+    simNotifier = make_unique<frc::Notifier>([this]
+                                             {
+        units::second_t const currentTime = utils::GetCurrentTime();
+        auto const deltaTime = currentTime - lastSimTime;
+        lastSimTime = currentTime;
+
+        /* use the measured time delta, get battery voltage from WPILib */
+        UpdateSimState(deltaTime, RobotController::GetBatteryVoltage()); });
+    simNotifier->StartPeriodic(kSimLoopPeriod);
+}
+
+void SwerveDrive::configureChoreo(PIDConstants translationPID,
+                                  PIDConstants rotationPID)
+{
+    choreo.xController = make_unique<PIDController>(
+        translationPID.kP, translationPID.kI, translationPID.kD);
+    choreo.yController = make_unique<PIDController>(
+        translationPID.kP, translationPID.kI, translationPID.kD);
+    choreo.headingController = make_unique<PIDController>(
+        rotationPID.kP, rotationPID.kI, rotationPID.kD);
+    choreo.headingController->EnableContinuousInput(-M_PI, M_PI);
+}
+
+void SwerveDrive::followTrajectory(const SwerveSample &sample)
+{
+    const auto &pose = GetState().Pose;
+    meters_per_second_t xFeedback{choreo.xController->Calculate(
+        pose.X().value(), sample.x.value())};
+    meters_per_second_t yFeedback{choreo.yController->Calculate(
+        pose.Y().value(), sample.y.value())};
+    radians_per_second_t headingFeedback{choreo.headingController->Calculate(
+        pose.Rotation().Radians().value(), sample.heading.value())};
+    SetControl(choreo.follower
+                   .WithSpeeds(ChassisSpeeds{
+                       xFeedback + sample.vx,
+                       yFeedback + sample.vy,
+                       headingFeedback + sample.omega}));
+}
+
+CommandPtr SwerveDrive::getSysIdRoutine()
+{
+    return cmd::Sequence(
+        cmd::RunOnce([]() {
+            SignalLogger::Start();
+        }),
+        sysIdRoutineTranslation.Quasistatic(Direction::kForward),
+        sysIdRoutineTranslation.Quasistatic(Direction::kReverse),
+        sysIdRoutineTranslation.Dynamic(Direction::kForward),
+        sysIdRoutineTranslation.Dynamic(Direction::kReverse),
+        sysIdRoutineSteerGains.Quasistatic(Direction::kForward),
+        sysIdRoutineSteerGains.Quasistatic(Direction::kReverse),
+        sysIdRoutineSteerGains.Dynamic(Direction::kForward),
+        sysIdRoutineSteerGains.Dynamic(Direction::kReverse),
+        sysIdRoutineRotation.Quasistatic(Direction::kForward),
+        sysIdRoutineRotation.Quasistatic(Direction::kReverse),
+        sysIdRoutineRotation.Dynamic(Direction::kForward),
+        sysIdRoutineRotation.Dynamic(Direction::kReverse),
+        cmd::RunOnce([]() {
+            SignalLogger::Stop();
+        })
+    );
+}
+
+template<typename RequestSupplier>
+    requires(!is_lvalue_reference_v<invoke_result_t<RequestSupplier>>) &&
+            requires(RequestSupplier req, SwerveDrive &drive) { drive.SetControl(req()); }
+CommandPtr SwerveDrive::applyRequest(RequestSupplier request)
+{
+    return cmd::Run([this, request = std::move(request)]() mutable
+                        {
+                            SetControl(request());
+                        }, this);
+}
+
+void SwerveDrive::Periodic()
+{
+}
+
+void SwerveDrive::AddVisionMeasurement(Pose2d pose, second_t timestamp)
+{
+    SwerveDrivetrain::AddVisionMeasurement(pose, utils::FPGAToCurrentTime(timestamp));
+}
+
+void SwerveDrive::setModuleOffsets(const std::array<Rotation2d, 4> &offsets)
+{
+    for (size_t i = 0; i < offsets.size(); ++i)
+    {
+        auto &module = GetModule(i);
+        auto& cancoder = module.GetEncoder();
+        configs::CANcoderConfiguration config;
+        cancoder.GetConfigurator().Refresh(config);
+        config.MagnetSensor.MagnetOffset = offsets[i].Degrees();
+        cancoder.GetConfigurator().Apply(config);
+    }
+}
+
+std::array<Rotation2d, 4> SwerveDrive::resetModuleOffsets(const std::array<frc::Rotation2d, 4>& targetOffsets)
+{
+    std::array<Rotation2d, 4> offsets;
+    for (size_t i = 0; i < offsets.size(); ++i)
+    {
+        auto &module = GetModule(i);
+        auto& cancoder = module.GetEncoder();
+        const auto& currentAngle = module.GetCurrentState().angle;
+        const auto& delta = currentAngle - targetOffsets[i];
+        configs::CANcoderConfiguration config;
+        cancoder.GetConfigurator().Refresh(config);
+        const auto& currentOffset = config.MagnetSensor.MagnetOffset;
+        auto newOffset = currentOffset + delta.Degrees();
+        newOffset = (degree_t)frc::InputModulus(((degree_t)newOffset).value(), -180.0, 180.0);
+        config.MagnetSensor.MagnetOffset = newOffset;
+        cancoder.GetConfigurator().Apply(config);
+        offsets[i] = Rotation2d{newOffset};
+    }
+    return offsets;
+}
---
+++ src/main/include/subsystems/SwerveDrive.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include <ctre/phoenix6/SignalLogger.hpp>
+#include <frc2/command/SubsystemBase.h>
+#include <ctre/phoenix6/swerve/SwerveDrivetrain.hpp>
+#include <units/time.h>
+#include <frc/Notifier.h>
+#include <frc2/command/sysid/SysIdRoutine.h>
+#include <choreo/Choreo.h>
+#include <frc/controller/PIDController.h>
+#include <pathplanner/lib/auto/AutoBuilder.h>
+#include <pathplanner/lib/controllers/PPHolonomicDriveController.h>
+#include <concepts>
+
+namespace nfr
+{
+    class SwerveDrive : public frc2::SubsystemBase, public ctre::phoenix6::swerve::SwerveDrivetrain<ctre::phoenix6::hardware::TalonFX, ctre::phoenix6::hardware::TalonFX, ctre::phoenix6::hardware::CANcoder>
+    {
+    private:
+        static constexpr units::second_t kSimLoopPeriod = 5_ms;
+        std::unique_ptr<frc::Notifier> simNotifier;
+        units::second_t lastSimTime;
+        static constexpr frc::Rotation2d kBlueAlliancePerspectiveRotation{0_deg};
+        static constexpr frc::Rotation2d kRedAlliancePerspectiveRotation{180_deg};
+        ctre::phoenix6::swerve::requests::SysIdSwerveTranslation translationCharacterization;
+        ctre::phoenix6::swerve::requests::SysIdSwerveSteerGains steerCharacterization;
+        ctre::phoenix6::swerve::requests::SysIdSwerveRotation rotationCharacterization;
+        frc2::sysid::SysIdRoutine sysIdRoutineTranslation{
+            frc2::sysid::Config{
+                std::nullopt, // Ramp rate of 1 volt / second
+                4_V,
+                std::nullopt, // 10 second timeout,
+                [](frc::sysid::State state)
+                {
+                    ctre::phoenix6::SignalLogger::WriteString("SysIdTranslation_State", frc::sysid::SysIdRoutineLog::StateEnumToString(state));
+                }},
+            frc2::sysid::Mechanism{
+                [this](units::volt_t output)
+                { SetControl(translationCharacterization.WithVolts(output)); },
+                {},
+                this}};
+        frc2::sysid::SysIdRoutine sysIdRoutineSteerGains{
+            frc2::sysid::Config{
+                std::nullopt, // Ramp rate of 1 volt / second
+                4_V,
+                std::nullopt, // 10 second timeout,
+                [](frc::sysid::State state)
+                {
+                    ctre::phoenix6::SignalLogger::WriteString("SysIdSteerGains_State", frc::sysid::SysIdRoutineLog::StateEnumToString(state));
+                }},
+            frc2::sysid::Mechanism{
+                [this](units::volt_t output)
+                { SetControl(steerCharacterization.WithVolts(output)); },
+                {},
+                this}};
+        frc2::sysid::SysIdRoutine sysIdRoutineRotation{
+            frc2::sysid::Config{
+                std::nullopt, // Ramp rate of 1 volt / second
+                4_V,
+                std::nullopt, // 10 second timeout,
+                [](frc::sysid::State state)
+                {
+                    ctre::phoenix6::SignalLogger::WriteString("SysIdRotation_State", frc::sysid::SysIdRoutineLog::StateEnumToString(state));
+                }},
+            frc2::sysid::Mechanism{
+                [this](units::volt_t output)
+                {
+                    SetControl(rotationCharacterization.WithRotationalRate(output * (1_rad_per_s / 1_V)));
+                    ctre::phoenix6::SignalLogger::WriteValue("Rotation_Rate", output * (1_rad_per_s / 1_V));
+                },
+                {},
+                this}};
+        ctre::phoenix6::swerve::requests::ApplyRobotSpeeds pathplannerFollower = ctre::phoenix6::swerve::requests::ApplyRobotSpeeds();
+        struct
+        {
+            ctre::phoenix6::swerve::requests::ApplyFieldSpeeds follower = ctre::phoenix6::swerve::requests::ApplyFieldSpeeds();
+            std::unique_ptr<frc::PIDController> xController;
+            std::unique_ptr<frc::PIDController> yController;
+            std::unique_ptr<frc::PIDController> headingController;
+        } choreo;
+        void configurePathplanner(pathplanner::PIDConstants translationPID, pathplanner::PIDConstants rotationPID);
+        void configureChoreo(pathplanner::PIDConstants translationPID, pathplanner::PIDConstants rotationPID);
+        void startSimThread();
+
+    public:
+        using SwerveModuleConstants = ctre::phoenix6::swerve::SwerveModuleConstants<
+            ctre::phoenix6::configs::TalonFXConfiguration,
+            ctre::phoenix6::configs::TalonFXConfiguration,
+            ctre::phoenix6::configs::CANcoderConfiguration>;
+        SwerveDrive(const ctre::phoenix6::swerve::SwerveDrivetrainConstants &drivetrainConstants,
+                    units::hertz_t updateRate,
+                    std::array<double, 3> const &odometryStandardDeviation,
+                    std::array<double, 3> const &visionStandardDeviation,
+                    pathplanner::PIDConstants translationPID,
+                    pathplanner::PIDConstants rotationPID,
+                    const SwerveModuleConstants & frontLeftConstants,
+                    const SwerveModuleConstants & frontRightConstants,
+                    const SwerveModuleConstants & backLeftConstants,
+                    const SwerveModuleConstants & backRightConstants);
+        frc2::sysid::SysIdRoutine &getSysIdTranslation() { return sysIdRoutineTranslation; }
+        frc2::sysid::SysIdRoutine &getSysIdSteerGains() { return sysIdRoutineSteerGains; }
+        frc2::sysid::SysIdRoutine &getSysIdRotation() { return sysIdRoutineRotation; }
+        frc2::CommandPtr getSysIdRoutine();
+        template <typename RequestSupplier>
+            requires(!std::is_lvalue_reference_v<std::invoke_result_t<RequestSupplier>>) &&
+                    requires(RequestSupplier req, SwerveDrive &drive) { drive.SetControl(req()); }
+        frc2::CommandPtr applyRequest(RequestSupplier request);
+        void Periodic() override;
+        void AddVisionMeasurement(frc::Pose2d pose, units::second_t timestamp) override;
+        void followTrajectory(const choreo::SwerveSample &sample);
+        void setModuleOffsets(const std::array<frc::Rotation2d, 4> &offsets);
+        std::array<frc::Rotation2d, 4> resetModuleOffsets(const std::array<frc::Rotation2d, 4> &targetOffsets);
+    };
+}
