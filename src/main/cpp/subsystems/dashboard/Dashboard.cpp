376a49d8e14fc1e3716be2f5774e5391dd5443c1
cml1010101 <cml1010101@gmail.com>
2025-08-02 12:53:38 -0400

    Add try/catch around webserver

--- src/main/cpp/subsystems/dashboard/Dashboard.cpp
+++ src/main/cpp/subsystems/dashboard/Dashboard.cpp
@@ -2,14 +2,31 @@
 
 #include <wpinet/WebServer.h>
 
+#include <iostream>
+
 namespace nfr
 {
 Dashboard::Dashboard(std::filesystem::path distPath, int port) : port(port)
 {
-    wpi::WebServer::GetInstance().Start(port, distPath.string());
+    try
+    {
+        wpi::WebServer::GetInstance().Start(port, distPath.string());
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to start web server: " << e.what() << std::endl;
+        throw;
+    }
 }
 Dashboard::~Dashboard()
 {
-    wpi::WebServer::GetInstance().Stop(port);
+    try
+    {
+        wpi::WebServer::GetInstance().Stop(port);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to stop web server: " << e.what() << std::endl;
+    }
 }
 }  // namespace nfr
